d91214025c5238e0a7e5045009e77e667ae31e9f
gabeFrancisco <gabegit2@gmail.com>
2025-03-18 14:08:33 -0300

    created User and UserResource

---
+++ src/main/java/com/ellyon/workshop_spring/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.ellyon.workshop_spring.resources;
+
+import com.ellyon.workshop_spring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Gabriel", "gabe@mail.com", "519987833", "1234");
+        return ResponseEntity.ok().body(u);
+    }
+}
