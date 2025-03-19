476be652da9349bc6a9a4894cb78e77a9ccba542
gabeFrancisco <gabegit2@gmail.com>
2025-03-18 21:46:20 -0300

    created findById service and resources methods

--- src/main/java/com/ellyon/workshop_spring/resources/UserResource.java
+++ src/main/java/com/ellyon/workshop_spring/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.ellyon.workshop_spring.resources;
 
 import com.ellyon.workshop_spring.entities.User;
+import com.ellyon.workshop_spring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Gabriel", "gabe@mail.com", "519987833", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
